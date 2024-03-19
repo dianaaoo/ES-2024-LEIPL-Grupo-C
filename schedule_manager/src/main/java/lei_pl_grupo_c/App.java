32055b6c4edcbb0c656bfefd84942ad2431430dc
Rui <160658326+rfflc@users.noreply.github.com>
2024-03-19 16:08:23 +0000

    Better CSV value reading

--- schedule_manager/src/main/java/lei_pl_grupo_c/App.java
+++ schedule_manager/src/main/java/lei_pl_grupo_c/App.java
@@ -1,5 +1,6 @@
 package lei_pl_grupo_c;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -12,6 +13,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
@@ -26,32 +28,47 @@ public class App {
 
     private static void readCSV() throws IOException {
 
-        String[] HEADERS = { "Curso", "Unidade Curricular", "Turno", "Turma", "Inscritos no turno", "Dia da semana",
-                "Hora início da aula", "Hora fima da aula", "Data da aula",
-                "Características da sala pedida para a aula", "Sala atribuída à aula" };
+        // String[] HEADERS = { "Curso", "Unidade Curricular", "Turno", "Turma",
+        // "Inscritos no turno", "Dia da semana",
+        // "Hora início da aula", "Hora fima da aula", "Data da aula",
+        // "Características da sala pedida para a aula", "Sala atribuída à aula" };
 
-        try (
-                Reader reader = Files.newBufferedReader(Paths.get("schedule_manager/src/samples/HorarioDeExemplo.csv"));
-                // salta os headers no ficheiro
-                CSVParser csvParser = new CSVParser(reader,
-                        CSVFormat.DEFAULT.builder().setHeader(HEADERS).setSkipHeaderRecord(true).build());) {
+        // try (
+        // Reader reader =
+        // Files.newBufferedReader(Paths.get("schedule_manager/src/samples/HorarioDeExemplo.csv"));
+        // // salta os headers no ficheiro
+        // CSVParser csvParser = new CSVParser(reader,
+        // CSVFormat.DEFAULT.builder().setHeader(HEADERS).setSkipHeaderRecord(true).build());)
+        // {
 
-            for (CSVRecord csvRecord : csvParser) {
+        // for (CSVRecord csvRecord : csvParser) {
 
-                // Accessing Values by Column Index
-                String curso = csvRecord.get(0);
-                // String unidadeCurricular = csvRecord.get(1);
-                // String turno = csvRecord.get(2);
-                // String turma = csvRecord.get(3);
+        // // Accessing Values by Column Index
+        // String curso = csvRecord.get(0);
+        // // String unidadeCurricular = csvRecord.get(1);
+        // // String turno = csvRecord.get(2);
+        // // String turma = csvRecord.get(3);
 
-                System.out.println("Curso: " + curso);
-                // System.out.println("Unidade curricular: " + unidadeCurricular);
-                // System.out.println("Turno: " + turno);
-                // System.out.println("Turma: " + turma);
+        // System.out.println("Curso: " + curso);
+        // // System.out.println("Unidade curricular: " + unidadeCurricular);
+        // // System.out.println("Turno: " + turno);
+        // // System.out.println("Turma: " + turma);
 
-            }
+        // }
 
+        // }
+
+        // T3
+
+        Scanner scanner = new Scanner(new File("schedule_manager/src/samples/HorarioDeExemplo.csv"));
+
+        scanner.useDelimiter(";"); // separa por ';'
+
+        while (scanner.hasNext()) {
+            System.out.print(scanner.next());
         }
+        
+        scanner.close();
 
     }
 }
