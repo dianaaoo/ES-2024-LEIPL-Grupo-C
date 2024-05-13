6028be7e9e0218d856fb08f9dc48527282cc226b
Rui <160658326+rfflc@users.noreply.github.com>
2024-05-13 17:12:05 +0100

    Revert "Merge pull request #19 from dianaaoo/LGC-44-Create-user-window-UI"
    This reverts commit 66ab397f26b481e4654ff506e79f4f1ac677916c, reversing
    changes made to 5c74350429529ad70a238066bc5dd50eb04dcc93.

--- src/main/java/lei_pl_grupo_c/App.java
+++ src/main/java/lei_pl_grupo_c/App.java
@@ -31,9 +31,11 @@ public class App {
         frame.setJMenuBar(menuBar);
 
         readCsvButton = new JButton("Read CSV");
-
+<<<<<<< HEAD
+=======
         saveCsvButton = new JButton("Save CSV");
         readJsonButton = new JButton("Read JSON");
+>>>>>>> main
         saveJsonButton = new JButton("Save JSON");
 
         readCsvButton.addActionListener(new ActionListener() {
